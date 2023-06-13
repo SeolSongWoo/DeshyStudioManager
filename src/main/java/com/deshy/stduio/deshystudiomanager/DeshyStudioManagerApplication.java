07b8b40d8f6f8719d624a63bb3686f165a5c3974
설송우 <pl9429@naver.com>
2023-06-13 12:50:48 +0900

    init Commit

---
+++ src/main/java/com/deshy/stduio/deshystudiomanager/DeshyStudioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.deshy.stduio.deshystudiomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeshyStudioManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeshyStudioManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deshy/stduio/deshystudiomanager/DeshyStudioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deshy.stduio.deshystudiomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeshyStudioManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
