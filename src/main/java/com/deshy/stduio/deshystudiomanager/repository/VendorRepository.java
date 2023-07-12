f5b5f51917723d2e74eda31b91a38eeae015758d
설송우 <pl9429@naver.com>
2023-07-12 16:16:46 +0900

    추가 : Products View 관련 컨트롤러, 서비스, DTO 추가 완료.

--- src/main/java/com/deshy/stduio/deshystudiomanager/repository/VendorRepository.java
+++ src/main/java/com/deshy/stduio/deshystudiomanager/repository/VendorRepository.java
@@ -1,12 +1,15 @@
 package com.deshy.stduio.deshystudiomanager.repository;
 
+import com.deshy.stduio.deshystudiomanager.data.entity.Member;
 import com.deshy.stduio.deshystudiomanager.data.entity.Vendor;
 import com.deshy.stduio.deshystudiomanager.repository.custom.VendorCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface VendorRepository extends JpaRepository<Vendor,Long>, VendorCustomRepository {
     Optional<Vendor> findByUid(UUID uid);
+    List<Vendor> findAllByMember(Member member);
 }
