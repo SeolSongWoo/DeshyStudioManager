dfbaace88482649ba6ed0c520c9f227e73535814
설송우 <pl9429@naver.com>
2023-08-21 18:38:05 +0900

    FEAT : 결제방법(카드, 현금, 계좌) 추가에 다른 백엔드 로직 수정.

--- src/main/java/com/deshy/stduio/deshystudiomanager/data/dto/vendor/VendorStoreDTO.java
+++ src/main/java/com/deshy/stduio/deshystudiomanager/data/dto/vendor/VendorStoreDTO.java
@@ -3,14 +3,18 @@ package com.deshy.stduio.deshystudiomanager.data.dto.vendor;
 import com.deshy.stduio.deshystudiomanager.data.dto.product.CategoryDTO;
 import com.deshy.stduio.deshystudiomanager.data.entity.ProductCategory;
 import com.deshy.stduio.deshystudiomanager.data.entity.VendorStore;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VendorStoreDTO {
     private String name;
     private UUID uid;
